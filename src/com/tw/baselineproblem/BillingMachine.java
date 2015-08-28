dbbf0e90c3caf8aa68b89199e91996408233dc9b
sophiag16 <sophia.g16@gmail.com>
2015-08-28 18:54:24 +0530

    added another case of producing spec for a single item list

--- src/com/tw/baselineproblem/BillingMachine.java
+++ src/com/tw/baselineproblem/BillingMachine.java
@@ -1,9 +1,19 @@
 package com.tw.baselineproblem;
 
 public class BillingMachine {
-    public static String produceReceipt(String itemList) {
-        return "1 book: 12.49\n" +
-                "Sales Taxes: 0.0\n" +
-                "Total: 12.49";
+    public static String produceReceipt(String itemInfo) {
+
+        String[] splitItemInfo = itemInfo.split(" ");
+        int length = splitItemInfo.length;
+        double shelfPrice = Double.parseDouble(splitItemInfo[length - 1]);
+        String receipt = "";
+
+        for(int i = 0; i < length - 2; i++) {
+            receipt += splitItemInfo[i] + " ";
+        }
+        
+        receipt += ": " + Double.toString(shelfPrice);
+        receipt += "\nSales Taxes : 0.0" + "\nTotal : " + Double.toString(shelfPrice);
+        return receipt;
     }
 }
--- test/com/tw/baselineproblem/TestBillingMachine.java
+++ test/com/tw/baselineproblem/TestBillingMachine.java
@@ -8,8 +8,19 @@ public class TestBillingMachine {
 
     @Test
     public void produceReceiptForASingleItemList() {
-        assertEquals("1 book: 12.49\n" +
-                "Sales Taxes: 0.0\n" +
-                "Total: 12.49", BillingMachine.produceReceipt("1 book at 12.49"));
+        BillingMachine billingMachine = new BillingMachine();
+
+        assertEquals("1 book : 12.49\n" +
+                "Sales Taxes : 0.0\n" +
+                "Total : 12.49", billingMachine.produceReceipt("1 book at 12.49"));
+    }
+
+    @Test
+    public void produceReceiptForAnotherSingleItemList() {
+        BillingMachine billingMachine = new BillingMachine();
+
+        assertEquals("1 chocolate bar : 0.85\n" +
+                "Sales Taxes : 0.0\n" +
+                "Total : 0.85", billingMachine.produceReceipt("1 chocolate bar at 0.85"));
     }
 }
